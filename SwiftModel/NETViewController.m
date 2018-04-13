a73d941296cbcac3c8ad136d4ec93162b93c6204
RM <RM@RM.local>
2018-04-13 14:44:38 +0800

    add json to Objective-C class

--- SwiftModel/NETViewController.m
+++ SwiftModel/NETViewController.m
@@ -8,6 +8,7 @@
 
 #import "NETViewController.h"
 
+
 @interface JsonType : NSObject
 ///，NET类型
 @property (nonatomic, copy) NSString *netType;
@@ -38,6 +39,7 @@
 @property (weak) IBOutlet NSTextView *netTF;
 @property (weak) IBOutlet NSTextField *prefixTF;
 @property (nonatomic, strong) NSMutableArray<JsonType *>  *typeArray;
+@property (nonatomic, strong) NSArray<NSString *>  *OCArray;
 @property (nonatomic, strong) NSMutableArray<Attribute *>  *mutableArray;
 @end
 
@@ -51,7 +53,7 @@
     NSArray *NETTypeArray = @[@"bool",@"decimal",@"double",@"float",@"int",@"long",@"String",@"DateTime",@"string"];
     NSArray *swiftTypeArray = @[@"Bool",@"Double",@"Double",@"Float",@"Int",@"Int",@"String",@"String",@"String"];
     NSArray *defaultValueArray = @[@"false",@"0",@"0",@"0",@"0",@"0",@"\"\"",@"\"\"",@"\"\""];
-    
+    self.OCArray = @[@"BOOL",@"double",@"double",@"float",@"NSInteger",@"NSInteger",@"NSString",@"NSString",@"NSString"];
     for (int i=0; i< NETTypeArray.count; i ++ ) {
         JsonType * type = [[JsonType alloc]init];
         type.netType = [NETTypeArray objectAtIndex:i];
@@ -63,7 +65,65 @@
     
     
 }
-
+///生成oc
+- (IBAction)okOCAction:(id)sender {
+    if (self.netTF.string.length==0) {
+        [self alertMsg:@"请输入要转换的json"];
+        return;
+    }
+    
+    NSString *json = [[[[[[self.netTF.string stringByReplacingOccurrencesOfString:@";" withString:@""]stringByReplacingOccurrencesOfString:@"get" withString:@""]stringByReplacingOccurrencesOfString:@"set" withString:@""]stringByReplacingOccurrencesOfString:@"{" withString:@""]stringByReplacingOccurrencesOfString:@"}" withString:@""]stringByReplacingOccurrencesOfString:@"///" withString:@""];
+    
+    NSArray * array = [json componentsSeparatedByString:@"<summary>"];
+    NSLog(@"%@", array);
+    NSString *attStr = [NSString stringWithFormat:@"@interface Model : %@ \n",self.prefixTF.stringValue.length?self.prefixTF.stringValue:@"NSObject"];
+    for (NSString *str in array) {
+        Attribute *att = [[Attribute alloc]init];
+        NSArray *subArr = [str componentsSeparatedByString:@"</summary>"];
+        ///去掉回车与tab
+        NSString *annotation = [[[subArr.firstObject stringByReplacingOccurrencesOfString:@"\n" withString:@""]stringByReplacingOccurrencesOfString:@"\r" withString:@""]stringByReplacingOccurrencesOfString:@" " withString:@""];
+        att.annotation = annotation ;
+        [self config:[[subArr.lastObject stringByReplacingOccurrencesOfString:@"\n" withString:@""]stringByReplacingOccurrencesOfString:@"\r" withString:@""] attribute:att];
+        if (att.Attribute) {
+            [self.mutableArray addObject:att];
+            if ([att.type isEqualToString:self.typeArray[0].swiftType] ||
+                [att.type isEqualToString:self.typeArray[2].swiftType] ||
+                [att.type isEqualToString:self.typeArray[3].swiftType] ||
+                [att.type isEqualToString:self.typeArray[4].swiftType] ||
+                [att.type isEqualToString:self.typeArray[5].swiftType] ||
+                [att.type isEqualToString:self.typeArray[6].swiftType] ||
+                [att.type isEqualToString:self.typeArray[7].swiftType] ||
+                [att.type isEqualToString:self.typeArray[8].swiftType] ) {
+                NSInteger index = 0;
+                for (JsonType *typ in self.typeArray) {
+                    if ([typ.swiftType isEqualToString:att.type]) {
+                        index = [self.typeArray indexOfObject:typ];
+                    }
+                }
+                if (index>5) {
+                    NSString *outStr = [NSString stringWithFormat:@"\n\r/**%@*/\r@property (nonatomic, copy) NSString *%@; ",att.annotation,att.Attribute];
+                    NSLog(@"%@", outStr);
+                    attStr = [NSString stringWithFormat:@"%@%@",attStr,outStr];
+                } else {
+                    NSString *outStr = [NSString stringWithFormat:@"\n\r/**%@*/\r@property (nonatomic, assign) %@ %@; ",att.annotation,[self.OCArray objectAtIndex:index],att.Attribute];
+                    NSLog(@"%@", outStr);
+                    attStr = [NSString stringWithFormat:@"%@%@",attStr,outStr];
+                }
+            } else {
+                NSString *outStr = [NSString stringWithFormat:@"\n\r/**%@*/\r@property (nonatomic, strong) %@ *%@; ",att.annotation,att.type,att.Attribute];
+                NSLog(@"%@", outStr);
+                attStr = [NSString stringWithFormat:@"%@%@",attStr,outStr];
+            }
+            
+        }
+        
+    }
+    self.netTF.string = attStr;
+    self.netTF.string = [NSString stringWithFormat:@"%@\n@end",self.netTF.string];
+    self.netTF.string = [NSString stringWithFormat:@"%@\n@implementation Model\n",self.netTF.string];
+    self.netTF.string = [NSString stringWithFormat:@"%@+ (NSDictionary *)modelContainerPropertyGenericClass {\n\rreturn @{ };\n}",self.netTF.string];
+    self.netTF.string = [NSString stringWithFormat:@"%@\n@end",self.netTF.string];
+}
 
 - (IBAction)okBtn:(id)sender {
     if (self.netTF.string.length==0) {
@@ -81,29 +141,23 @@
         NSArray *subArr = [str componentsSeparatedByString:@"</summary>"];
         ///去掉回车与tab
         NSString *annotation = [[[subArr.firstObject stringByReplacingOccurrencesOfString:@"\n" withString:@""]stringByReplacingOccurrencesOfString:@"\r" withString:@""]stringByReplacingOccurrencesOfString:@" " withString:@""];
-        att.annotation = annotation;
-
-        
+        att.annotation = annotation ;
         [self config:[[subArr.lastObject stringByReplacingOccurrencesOfString:@"\n" withString:@""]stringByReplacingOccurrencesOfString:@"\r" withString:@""] attribute:att];
-        [self.mutableArray addObject:att];
-        NSString *outStr = [NSString stringWithFormat:@"\r/**%@*/\rpublic var %@ : %@ = %@",att.annotation,att.Attribute,att.type,att.defaultValue];
-        NSLog(@"%@", outStr);
-        attStr = [NSString stringWithFormat:@"%@\n%@",attStr,outStr];
+        if (att.Attribute) {
+            [self.mutableArray addObject:att];
+        
+            NSString *outStr = [NSString stringWithFormat:@"\r/**%@*/\rpublic var %@ : %@ = %@",att.annotation,att.Attribute,att.type,att.defaultValue];
+            NSLog(@"%@", outStr);
+            attStr = [NSString stringWithFormat:@"%@%@",attStr,outStr];
+        }
         
     }
     NSString *supModel = self.prefixTF.stringValue.length ? self.prefixTF.stringValue : @"Codable";
     self.netTF.string = [NSString stringWithFormat:@"public class Model : %@ {\n %@ ",supModel,attStr];
-    /**
-    private enum CodingKeys : String, CodingKey {
-    case name
-    case abc = "alc_v"
-    case brewery
-    case style
-    }
-    */
+
     self.netTF.string = [NSString stringWithFormat:@"%@\n\rprivate enum CodingKeys : String, CodingKey {\n",self.netTF.string];
     for (Attribute *model in self.mutableArray) {
-        self.netTF.string = [NSString stringWithFormat:@"%@\n\rcase %@ = \"%@\"",self.netTF.string,model.Attribute,model.Attribute];
+        self.netTF.string = [NSString stringWithFormat:@"%@\rcase %@ = \"%@\"",self.netTF.string,model.Attribute,model.Attribute];
     }
     self.netTF.string = [NSString stringWithFormat:@"%@\n\r}",self.netTF.string];
     self.netTF.string = [NSString stringWithFormat:@"%@\n}",self.netTF.string];
@@ -111,27 +165,48 @@
 
 
 - (void)config:(NSString *)str attribute:(Attribute *)attribute{
+    while ([str hasPrefix:@"\r"]) {
+        
+        str = [str substringFromIndex:1];
+    }
+    while ([str hasSuffix:@"\r"]) {
+        
+        str = [str substringToIndex:([str length] - 1)];
+    }
+    while ([str hasPrefix:@" "]) {
+        
+        str = [str substringFromIndex:1];
+    }
+    while ([str hasSuffix:@" "]) {
+        
+        str = [str substringToIndex:([str length] - 1)];
+    }
     NSArray *subArr = [str componentsSeparatedByString:@" "];
     
     for (NSString *s in subArr) {
         if (s.length>0) {
-            if ([s isEqualToString:@"public"]) {
-            } else if ([s isEqualToString:self.typeArray[0].netType] ||
-                       [s isEqualToString:self.typeArray[2].netType] ||
-                       [s isEqualToString:self.typeArray[3].netType] ||
-                       [s isEqualToString:self.typeArray[4].netType] ||
-                       [s isEqualToString:self.typeArray[5].netType] ||
-                       [s isEqualToString:self.typeArray[6].netType] ||
-                       [s isEqualToString:self.typeArray[7].netType] ||
-                       [s isEqualToString:self.typeArray[8].netType] ){
-                for (JsonType *type in self.typeArray) {
-                    if ([s isEqualToString:type.netType]) {
-                        attribute.type = type.swiftType;
-                        attribute.defaultValue = type.defaultValue;
-                    }
-                }
-            } else {
+            if ([subArr.firstObject isEqualToString:s]) {
+            } else  if ([subArr.lastObject isEqualToString:s]){
                 attribute.Attribute = s;
+            } else {
+                if ([s isEqualToString:self.typeArray[0].netType] ||
+                         [s isEqualToString:self.typeArray[2].netType] ||
+                         [s isEqualToString:self.typeArray[3].netType] ||
+                         [s isEqualToString:self.typeArray[4].netType] ||
+                         [s isEqualToString:self.typeArray[5].netType] ||
+                         [s isEqualToString:self.typeArray[6].netType] ||
+                         [s isEqualToString:self.typeArray[7].netType] ||
+                         [s isEqualToString:self.typeArray[8].netType] ){
+                    for (JsonType *type in self.typeArray) {
+                        if ([s isEqualToString:type.netType]) {
+                            attribute.type = type.swiftType;
+                            attribute.defaultValue = type.defaultValue;
+                        }
+                    }
+                } else {
+                    attribute.type = s;
+                    attribute.defaultValue = [NSString stringWithFormat:@"%@()",s];
+                }
             }
         }
     }
